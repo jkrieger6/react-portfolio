9ed23ce0e9fb48303068536959a5514580feb9d2
jkrieger6 <jkrieger11@gmail.com>
2023-07-11 16:02:26 -0700

    added footer to page

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer>
+            <ul className="footer">
+                <li className="footer">
+                    <a href="">GitHub</a>
+                </li>
+                <li className="footer">
+                    <a href="">LinkedIn</a>
+                </li>
+            </ul>
+        </footer>
+    );
+}

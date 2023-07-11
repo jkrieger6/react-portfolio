d06b9a6f75fac3a768d9c74b93ef1ed1c4a28d9b
jkrieger6 <jkrieger11@gmail.com>
2023-07-11 16:03:31 -0700

    added resume page to portfolio

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Resume() {
+    return (
+        <div>
+            <h1>Resume</h1>
+            <p>Download my <a href="https://docs.google.com/document/d/1k5oJ6s8X8VX8Dd5j0qJ3Q8Jy8p8uJ7X0/edit?usp=sharing&ouid=105958420065297075497&rtpof=true&sd=true">resume</a>.</p>
+            <h2>Front-end Proficiencies</h2>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>jQuery</li>
+                <li>responsive design</li>
+                <li>React</li>
+                <li>Bootstrap</li>
+            </ul>
+            <h2>Back-end Proficiencies</h2>
+            <ul>
+                <li>APIs</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>MySQL, Sequelize</li>
+                <li>MongoDB, Mongoose</li>
+                <li>REST</li>
+                <li>GraphQL</li>
+            </ul>
+        </div>
+    );
+}

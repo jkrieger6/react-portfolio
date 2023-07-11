d13aa37e5c7a4e52fe3610b3de36c93f2d7acf20
jkrieger6 <jkrieger11@gmail.com>
2023-07-11 16:00:49 -0700

    added about me section

---
+++ src/components/pages/AboutMe.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function AboutMe() {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <img src="" alt="Portrait picture of myself at the Grand Canyon" />
+            <p>
+                I am a full stack web developer with a full stack web development bootcamp certificate. 
+                I have a passion for creating and learning new things.
+                I am a team player and I am excited to bring my skills to a new team. 
+                I am a hard worker and I am always looking for ways to improve my skills.
+                I am a fast learner and I am excited to learn new skills.
+            </p>
+        </div>
+    );
+}

a45914cb79947897359ebb1e8f961a6e03188699
jkrieger6 <jkrieger11@gmail.com>
2023-07-11 15:51:47 -0700

    added header and footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
+import Header from "./components/Header";
+import Footer from "./components/Footer.js";
 
-const app = () => <PortfolioContainer />;
+function app () {
+    return (
+        <>
+         <Header />
+<PortfolioContainer />
+<Footer />
+        </>
+       
+
+    )
+} 
 
 export default app;

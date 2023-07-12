ccbd176d2cdb52863d11c7acee41d28bfabb7c5c
jkrieger6 <jkrieger11@gmail.com>
2023-07-12 14:56:13 -0700

    added links to footer for linked in and github

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,10 @@ export default function Footer() {
         <footer>
             <ul className="footer">
                 <li className="footer">
-                    <a href="">GitHub</a>
+                    <a href="https://github.com/jkrieger6">GitHub</a>
                 </li>
                 <li className="footer">
-                    <a href="">LinkedIn</a>
+                    <a href="https://www.linkedin.com/in/jacob-krieger-043066b8/">LinkedIn</a>
                 </li>
             </ul>
         </footer>

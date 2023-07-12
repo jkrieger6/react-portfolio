6c16b353cb269a70d02332082349efa316d17ad0
jkrieger6 <jkrieger11@gmail.com>
2023-07-12 15:24:39 -0700

    added styling to about me section

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,11 +1,39 @@
 import React from "react";
 
+const styles = {
+    aboutMeStyles: {
+        width: "100%",
+        height: "100%",
+        backgroundColor: "grey",
+        color: "ivory",
+        textAlign: "center",
+        padding: "20px",
+        margin: "20px",
+        border: "5px solid black",
+    },
+    imgStyles: {
+        width: "50%",
+        height: "50%",
+        border: "5px solid black",
+    },
+    pStyles: {
+        width: "100%",
+        height: "100%",
+        backgroundColor: "grey",
+        color: "ivory",
+        textAlign: "center",
+        padding: "20px",
+        margin: "20px",
+        border: "5px solid black",
+    },
+};
+
 export default function AboutMe() {
     return (
-        <div>
+        <div style={styles.aboutMeStyles}>
             <h1>About Me</h1>
-            <img src="" alt="Portrait picture of myself at the Grand Canyon" />
-            <p>
+            <img style={styles.imgStyles} src="" alt="Portrait picture of myself at the Grand Canyon" />
+            <p style={styles.pStyles}>
                 I am a full stack web developer with a full stack web development bootcamp certificate. 
                 I have a passion for creating and learning new things.
                 I am a team player and I am excited to bring my skills to a new team. 

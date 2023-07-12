d06e8a1474810a8515033ef7ed9eac321b67c5aa
jkrieger6 <jkrieger11@gmail.com>
2023-07-12 15:31:31 -0700

    added styling to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 import React from "react";
 
+const styles = {
+    headerStyles: {
+        width: "100%",
+        backgroundColor: "grey",
+        color: "ivory",
+        textAlign: "center",
+        padding: "40px",
+        margin: "20px",
+},
+};
 
 export default function Header() {
     return (
-        <header>
+        <header style={styles.headerStyles}>
             <h1>Jacob Krieger</h1>
         </header>
     );

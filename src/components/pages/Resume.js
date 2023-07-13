e00e7a64020833a492ec9f5f1405f90c7cc01a1b
jkrieger6 <jkrieger11@gmail.com>
2023-07-13 16:41:46 -0700

    added some basic styling to page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,11 +1,40 @@
 import React from "react";
 
+const styles = {
+    resumeStyles: {
+        height: "100%",
+        backgroundColor: "grey",
+        color: "ivory",
+        textAlign: "center",
+        border: "5px solid black",
+    },
+    pStyles: {
+        height: "100%",
+        backgroundColor: "grey",
+        color: "ivory",
+        textAlign: "center",
+        padding: "20px",
+        margin: "20px",
+        border: "5px solid black",
+    },
+    skillsStyles: {
+        height: "100%",
+        backgroundColor: "grey",
+        color: "ivory",
+        textAlign: "center",
+        padding: "20px",
+        margin: "20px",
+        border: "5px solid black",
+    },
+
+};
+
 export default function Resume() {
     return (
         <div>
             <h1>Resume</h1>
-            <p>Download my <a href="https://docs.google.com/document/d/1aD1lK1apb2Pq_5YiKVMDHzPSd2zgBIpg8Jjvgl_Ew4k/edit?usp=sharing">resume</a>.</p>
-            <h2>Front-end Proficiencies</h2>
+            <p style={styles.pStyles} >Download my <a href="https://docs.google.com/document/d/1aD1lK1apb2Pq_5YiKVMDHzPSd2zgBIpg8Jjvgl_Ew4k/edit?usp=sharing">resume here!</a>.</p>
+            <h2 style={styles.skillsStyles}>Front-end Proficiencies</h2>
             <ul>
                 <li>HTML</li>
                 <li>CSS</li>
@@ -15,7 +44,7 @@ export default function Resume() {
                 <li>React</li>
                 <li>Bootstrap</li>
             </ul>
-            <h2>Back-end Proficiencies</h2>
+            <h2 style={styles.skillsStyles}>Back-end Proficiencies</h2>
             <ul>
                 <li>APIs</li>
                 <li>Node</li>

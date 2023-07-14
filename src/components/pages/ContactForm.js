75cdb794ec068df3243088e1e6be248b86c104ae
jkrieger6 <jkrieger11@gmail.com>
2023-07-14 10:56:27 -0700

    added error messages if fields were left blank for contact form

--- src/components/pages/ContactForm.js
+++ src/components/pages/ContactForm.js
@@ -18,6 +18,7 @@ divStyles: {
 },
 };
 
+// Function to render error message if email is not valid
 export default function ContactForm() {
     const [ emailError, setEmailError ] = useState(false)
     const handleBlur = (e) => {
@@ -26,13 +27,37 @@ export default function ContactForm() {
             setEmailError(true)
         }
     } 
+
+    // Function to render error message if name field is left blank
+    const [ nameError, setNameError ] = useState(false)
+    const handleNameBlur = (e) => {
+        const name = e.target.name;
+        if (name === "name" && !e.target.value){
+            setNameError(true)
+        }
+    }
+
+    // Function to render error message if message field is left blank
+    const [ messageError, setMessageError ] = useState(false)
+    const handleMessageBlur = (e) => {
+        const name = e.target.name;
+        if (name === "message" && !e.target.value){
+            setMessageError(true)
+        }
+    }
+
     return (
         <div>
             <h1 style={styles.h1Styles}>Contact Me</h1>
             <form style={styles.formStyles}>
                 <div style={styles.divStyles} className="form-group">
                     <label for="FormControlInput1">Name</label>
-                    <input type="name" className="form-control" id="FormControlInput1" placeholder="Name" />
+                    <input name="name" type="name" onBlur={handleNameBlur} className="form-control" id="FormControlInput1" placeholder="Name" />
+                    {
+                        nameError && (
+                            <span>Please provide your name.</span>
+                        )
+                    }
                 </div>
                 <div style={styles.divStyles} className="form-group">
                     <label for="FormControlInput2">Email address</label>
@@ -45,7 +70,12 @@ export default function ContactForm() {
                 </div>
                 <div style={styles.divStyles} className="form-group">
                     <label for="FormControlTextarea1">Message</label>
-                    <textarea className="form-control" id="FormControlTextarea1" rows="3"></textarea>
+                    <textarea name="message" onBlur={handleMessageBlur} className="form-control" id="FormControlTextarea1" rows="3"></textarea>
+                    {
+                        messageError && (
+                            <span>Please provide a message.</span>
+                        )
+                    }
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>

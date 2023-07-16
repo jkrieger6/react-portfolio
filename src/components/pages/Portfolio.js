90788209daa5f86332def3677865da88244e311d
jkrieger6 <jkrieger11@gmail.com>
2023-07-16 14:08:06 -0700

    moved some stuff around to look more professional

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -32,20 +32,25 @@ const styles = {
         display: 'flex',
         margin: '0 auto',
     },
+    projectStyles: {
+        textDecoration: "underline",
+        fontSize: "30px",
+    },
 }
 
 // Single component to be reused for multiple projects
  function Project(props) {
     return (
         <div style={styles.card} className="card-container">
-            <div className="card-container">
-                <img style={styles.image} alt={props.data.name} src={props.data.image} />
-            </div>
+            
             <div style={styles.content} className="card-body">
                 <p>
-                    <li>
-                        Name: {props.data.name}
-                    </li>
+                    <div style={styles.projectStyles}>
+                        {props.data.name}
+                    </div>
+                    <div className="card-container">
+                <img style={styles.image} alt={props.data.name} src={props.data.image} />
+            </div>
                     {/*if there is a deployed link, show it, otherwise show nothing */}
                     <li>
                          {/* <script>
@@ -53,11 +58,11 @@ const styles = {
                             props.data.deployed = "No deployed link available"
                         }
                     </script> */}
-                        Deployed Application Link: {props.data.deployed}
+                        Link to Deployed Application: {props.data.deployed}
                         {/* <p style={styles.pStyles} >Deployed Application <a href="{props.data.deployed}">here!</a></p> */}
                     </li>
                     <li>
-                        GitHub Repository Link: {props.data.github}
+                        Link to GitHub repository: {props.data.github}
                     </li>
                 </p>
             </div>
@@ -71,36 +76,36 @@ export default function Portfolio() {
             name: "Beer Traveler",
             image: beerTraveler,
             deployed: "https://jkrieger6.github.io/beer-traveler/",
-            github:""
+            github:"https://github.com/jkrieger6/beer-traveler"
         },
         {
             name: "The Recipe Drawer",
             image: recipeDrawer,
             deployed: "https://the-recipe-drawer.herokuapp.com",
-            github: ""
+            github: "https://github.com/BigMikeNova/the-recipe-drawer"
         },
         {
             name: "Work Day Scheduler",
             image: workDay,
-            deployed: "https://jkrieger6.github.io/work-day-scheduler/",
-            github: ""
+            deployed: " https://jkrieger6.github.io/Work-Day-Calendar/",
+            github: "https://github.com/jkrieger6/Work-Day-Calendar"
         },
         {
             name: "README Generator",
             image: readmeGen,
-            deployed: "",
-            github: "https://github.com/jkrieger6/README-generator/blob/main/README.md"
+            deployed: "https://github.com/jkrieger6/README-generator",
+            github: "https://github.com/jkrieger6/README-generator"
         },
         {
             name: "Employee Tracker",
             image: employeeTracker,
-            deployed: "",
-            github: "https://github.com/jkrieger6/Employee-tracker.git"
+            deployed: "https://github.com/jkrieger6/Employee-tracker",
+            github: "https://github.com/jkrieger6/Employee-tracker"
         },
         {
             name: "SVG Generator",
             image: svg,
-            deployed: "",
+            deployed: "https://github.com/jkrieger6/brand-logo-generator",
             github: "https://github.com/jkrieger6/brand-logo-generator"
         }
     ]

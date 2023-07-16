312348e3de68045f7e2e1a15c0d6b6e5bdd86d22
jkrieger6 <jkrieger11@gmail.com>
2023-07-16 12:59:03 -0700

    added image and styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,26 @@
 import React from "react";
+import headerBg from "../images/headerBg.jpeg";
 
 const styles = {
     headerStyles: {
         backgroundColor: "grey",
-        // color: "ivory",
-        textAlign: "center",
+        color: "#C2C2C2",
         padding: "40px",
+        position: "relative",
+        justifyContent: "center",
+        display: "flex",
 },
+imageStyles: {
+    width: "100%",
+    height: "140px",
+    position: "absolute",
+    top: "0",
+    left: "0",
+},
+textStyles: {
+    position: "relative",
+    zIndex: "1",
+}
 };
 
 
@@ -14,10 +28,10 @@ const styles = {
 export default function Header() {
     return (
         <header style={styles.headerStyles}>
+            <div style={styles.textStyles}>
             <h1> Hello There! I'm Jacob Krieger</h1>
-            <div>
-                 <img src="/images/headerBg.jpeg" alt="background" /> 
             </div>
+                 <img style={styles.imageStyles} src={headerBg} alt="background" /> 
         </header>
     );
 }

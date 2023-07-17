47c4429ca1f56eaceb39701ddf162a9f48c636fe
jkrieger6 <jkrieger11@gmail.com>
2023-07-17 09:06:37 -0700

    restyled about me page to look cleaner

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,48 +1,49 @@
 import React from "react";
 import selfie from "../../images/IMG_3883 Small.jpeg";
+import 'animate.css'
 
 const styles = {
     aboutMeStyles: {
-        height: "100%",
-        backgroundColor: "grey",
-        color: "#9CF6F6",
-        textAlign: "center",
-        border: "5px solid black",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-    },
-    imgStyles: {
-        width: "50%",
-        height: "50%",
-        border: "5px solid black",
-    },
-    pStyles: {
-        height: "100%",
-        backgroundColor: "grey",
-        color: "#9CF6F6",
-        textAlign: "center",
-        padding: "20px",
+        backgroundColor: "#C2C2C2",
+        color: "#113e7c",
+        border: "5px solid green",
         margin: "20px",
-        border: "5px solid black",
-        display: "flex",
-
     },
-};
+    body: {
+        backgroundColor: "grey",
+    },
+}
 
 export default function AboutMe() {
-    return (
-        <div style={styles.aboutMeStyles}>
-            <h1>About Me</h1>
-            <img style={styles.imgStyles} src={selfie} alt="Portrait of myself at the Grand Canyon" />
-            <div class="container" style={styles.pStyles}>
-                I am a full stack web developer with a full stack web development bootcamp certificate. 
-                I have a passion for creating and learning new things.
-                I am a team player and I am excited to bring my skills to a new team. 
-                I am a hard worker and I am always looking for ways to improve my skills.
-                I am a fast learner and eager to learn.
-                Some hobbies I enjoy are hiking, camping, climbing, playing soccer and anything that has to do with nature!
+  return (
+    <div style={styles.body}>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-8">
+          <div style={styles.aboutMeStyles} className="card mb-3">
+            <div className="card-body text-center">
+              <h1 className="card-title">About Me</h1>
+              <div class="animate__animated animate__jello">
+              <img
+                className="img-fluid rounded-circle"
+                src={selfie}
+                alt="Portrait of myself at the Grand Canyon"
+              />
+              </div>
+              <p className="card-text mt-4">
+                I am a full stack web developer with a full stack web development
+                bootcamp certificate. I have a passion for creating and learning
+                new things. I am a team player and I am excited to bring my skills
+                to a new team. I am a hard worker and I am always looking for ways
+                to improve my skills. I am a fast learner and eager to learn. Some
+                hobbies I enjoy are hiking, camping, climbing, playing soccer and
+                anything that has to do with nature!
+              </p>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+    </div>
+  );
 }
